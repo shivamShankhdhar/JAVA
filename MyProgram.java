330b361678408cc23658e181a9fbf463e51e6bb3
shivam shankhdhar <s.shankhdhar1981@gmail.com>
2021-01-23 19:04:34 +0530

    First commit

---
+++ MyProgram.java
@@ -0,0 +1,12 @@
+// package introduction;
+
+public class MyProgram{
+	public static void main(String [] args){
+		System.out.println("Hello this is shivam");
+		System.out.println("Hello this is shivam");
+		System.out.println("Hello this is shivam");
+		System.out.println("Hello this is shivam");
+		System.out.println("Hello this is shivam");
+	}
+}
+
